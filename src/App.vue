31efaf8bfc516da3e727bcab9c94b478ec6e6aff
fabiomilheiro <fabiomilheiro@gmail.com>
2024-11-12 12:51:35 +0000

    -

--- src/App.vue
+++ src/App.vue
@@ -72,12 +72,12 @@ const onSelected = (v) => {
       </ComboboxAnchor>
 
       <ComboboxContent
-        class="absolute z-10 w-full mt-2 min-w-[160px] bg-white overflow-hidden rounded shadow-[0px_10px_38px_-10px_rgba(22,_23,_24,_0.35),_0px_10px_20px_-15px_rgba(22,_23,_24,_0.2)] will-change-[opacity,transform] data-[side=top]:animate-slideDownAndFade data-[side=right]:animate-slideLeftAndFade data-[side=bottom]:animate-slideUpAndFade data-[side=left]:animate-slideRightAndFade"
+        class="absolute z-10 mt-2 min-w-12 bg-white overflow-hidden rounded shadow-[0px_10px_38px_-10px_rgba(22,_23,_24,_0.35),_0px_10px_20px_-15px_rgba(22,_23,_24,_0.2)] will-change-[opacity,transform] data-[side=top]:animate-slideDownAndFade data-[side=right]:animate-slideLeftAndFade data-[side=bottom]:animate-slideUpAndFade data-[side=left]:animate-slideRightAndFade"
       >
         <ComboboxViewport class="p-[5px]">
           <ComboboxEmpty
-            class="text-mauve8 text-xs font-medium text-center py-2"
-          />
+            class="text-mauve8 text-xs font-medium text-center p-2"
+          >No options</ComboboxEmpty>
 
           <ComboboxGroup>
             <ComboboxLabel
